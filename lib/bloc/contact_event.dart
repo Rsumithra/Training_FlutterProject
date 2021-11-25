cec438084763903b185e46076ff80a9facf6b41f
Sumithra <sumithra.r@marketsimplified.com>
2021-11-25 11:07:06 +0530

    Contact initial commit

--- lib/bloc/contact_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'contact_bloc.dart';
-
-@immutable
-abstract class ContactEvent {
-  
-}
--- lib/bloc/contact_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'contact_bloc.dart';
-
-@immutable
-abstract class ContactState {}
-
-class ContactInitial extends ContactState {
-  
-}
